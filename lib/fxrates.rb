61a614d3549ceff52ecf81714dc37cc04bb78f4d
Stuart Fraser <stuartfraser@Stuarts-MacBook-Pro.local>
2018-02-02 11:07:42 +0000

    Tidy up

--- lib/fxrates.rb
+++ lib/fxrates.rb
@@ -18,7 +18,8 @@ class ExchangeRate
 		response = Crack::XML.parse(File.read(open("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")))
 	 	File.open("lib/rates.json","w") do |f|
   		f.write(JSON.pretty_generate(response))
-		
+  		end
+
 		file = File.read('./rates.json')
 		data = JSON.parse(file)['gesmes:Envelope']['Cube']['Cube']
 		found = false
